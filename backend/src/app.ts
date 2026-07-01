1525f7504d0e7ff5b0f4c159155d890ffc0fb746
malannly <mamedovaanna.i@gmail.com>
2026-07-01 14:31:16 +0300

    fixing bugs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,6 @@ import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
-import { csrfProtection } from 'middlewares/csrf'
 
 const { PORT = 3000 } = process.env
 const app = express()
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -10,13 +10,13 @@ import {
     updateCurrentUser
 } from '../controllers/auth'
 import auth from '../middlewares/auth'
-import { csrfProtection } from 'middlewares/csrf'
+import { csrfProtection } from '../middlewares/csrf'
 
 const authRouter = Router()
 
 authRouter.get('/csrf-token', getCsrfToken)
 authRouter.get('/user', auth, getCurrentUser)
-authRouter.patch('/me', csrfProtection, updateCurrentUser)
+authRouter.patch('/me', auth, csrfProtection, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
 authRouter.post('/login', csrfProtection, login)
 authRouter.get('/token', refreshAccessToken)

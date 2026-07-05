d05a261c173a6e38265052c77e1acbbc35a687a6
malannly <mamedovaanna.i@gmail.com>
2026-07-05 20:02:42 +0300

    commet fix

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -6,6 +6,7 @@ import NotFoundError from '../errors/not-found-error'
 import Order, { IOrder } from '../models/order'
 import Product, { IProduct } from '../models/product'
 import User from '../models/user'
+import sanitizeHtml from 'sanitize-html'
 
 // eslint-disable-next-line max-len
 // GET /orders?page=2&limit=5&sort=totalAmount&order=desc&orderDateFrom=2024-07-01&orderDateTo=2024-08-01&status=delivering&totalAmountFrom=100&totalAmountTo=1000&search=%2B1
@@ -312,13 +313,18 @@ export const createOrder = async (
 
         const safePhone = String(phone).replace(/\D/g, '')
 
+        const safeComment = sanitizeHtml(comment || '', {
+            allowedTags: [],
+            allowedAttributes: {},
+        })
+
         const newOrder = new Order({
             totalAmount: total,
             products: items,
             payment,
             phone: safePhone,
             email,
-            comment,
+            comment: safeComment,
             customer: userId,
             deliveryAddress: address,
         })

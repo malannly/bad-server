f4be6c2d518f668f4fb57d362f1a5b6f1032f6f5
malannly <mamedovaanna.i@gmail.com>
2026-07-06 17:38:17 +0300

    fix customer limit

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,6 +7,7 @@ import customerRouter from './customers'
 import orderRouter from './order'
 import productRouter from './product'
 import uploadRouter from './upload'
+import { apiLimiter } from '../middlewares/limit'
 
 const router = Router()
 
@@ -14,7 +15,7 @@ router.use('/auth', authRouter)
 router.use('/product', productRouter)
 router.use('/order', auth, orderRouter)
 router.use('/upload', auth, uploadRouter)
-router.use('/customers', auth, customerRouter)
+router.use('/customers', auth, apiLimiter, customerRouter)
 
 router.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new NotFoundError('Маршрут не найден'))

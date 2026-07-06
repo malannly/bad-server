6336cb87689d5a224ab43adb894f6a02d046dec1
malannly <mamedovaanna.i@gmail.com>
2026-07-06 11:33:55 +0300

    fix the filename

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -2,6 +2,7 @@ import { Request, Express } from 'express'
 import multer, { FileFilterCallback } from 'multer'
 import { mkdirSync } from 'fs'
 import path, { join } from 'path'
+import { v4 as uuidv4 } from 'uuid';
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
@@ -29,8 +30,10 @@ const storage = multer.diskStorage({
         file: Express.Multer.File,
         cb: FileNameCallback
     ) => {
-        cb(null, path.basename(file.originalname))
-    },
+        const ext = path.extname(file.originalname)
+        const fileName = `${uuidv4()}${ext}`
+        cb(null, fileName)
+    }
 })
 
 const types = [
